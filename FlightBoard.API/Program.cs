4fb6f2464074e8e316c35a61a63450b697c7fd8f
NaorOrZion <naor5506@gmail.com>
2025-07-19 20:14:45 +0300

    Add CORS policy for localhost:3000 and include database files

--- FlightBoard.API/Program.cs
+++ FlightBoard.API/Program.cs
@@ -19,10 +19,25 @@ Console.WriteLine("DB Connection String: " + (builder.Configuration.GetConnectio
 // Add controllers
 builder.Services.AddControllers();
 
+// Add CORS policy to allow requests from localhost:3000
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocal3000", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
 // Add SignalR services
 builder.Services.AddSignalR();
 
 var app = builder.Build(); // Build the web application
+
+// Use the CORS policy before mapping controllers
+app.UseCors("AllowLocal3000");
 
 app.MapControllers(); // Use controllers for HTTP requests
 
